462e242ec6b2a55b50e924212d39967175d12425
Roberto Martins <robertoalmartins@yahoo.com.br>
2015-01-21 01:18:47 -0200

    Changes in backend directories

--- mobile/www/src/controller/Provider.js
+++ mobile/www/src/controller/Provider.js
@@ -3,18 +3,54 @@
  */
 function loadProvider() {
     var success = function(response) {
-        var data = jQuery.parseJSON(response);
+        var providerList = jQuery.parseJSON(response);
 
-        for(var i in data.dataset){
-            var provider = data.dataset[i];
-            $('#providerlist tbody'). append(
-                '<tr id="tr_id'+provider.id+'" class="tr-class-'+provider.id+'">'+
-                '<td id="td_id_'+provider.id+'" class="td-class-'+provider.id+'">'+provider.id+'</td>'+
-                '<td>'+provider.name+'</td>'+
-                '<td>'+provider.city+'</td>'+
-                '</tr>'
-            );
-        }
+        $('#providerTable').bootstrapTable({
+            data: providerList.dataset,
+            cache: false,
+            height: 400,
+            striped: true,
+            pagination: false,
+            pageSize: 50,
+            pageList: [10, 25, 50, 100, 200],
+            search: true,
+            showColumns: true,
+            showRefresh: true,
+            minimumCountColumns: 2,
+            clickToSelect: true,
+            smartDisplay: true,
+            clickToSelect: true,
+            singleSelect: true,
+            columns: [{
+                field: 'id',
+                title: 'ID',
+                align: 'right',
+                valign: 'bottom',
+                sortable: true
+            }, {
+                field: 'name',
+                title: 'Nome',
+                align: 'left',
+                valign: 'middle',
+                sortable: true
+            }, {
+                field: 'city',
+                title: 'Cidade',
+                align: 'left',
+                valign: 'top',
+                sortable: true
+            }]
+        });
+
+        //for(var i in data.dataset){
+        //    var provider = data.dataset[i];
+        //    $('#providerlist tbody'). append(
+        //        '<tr id="tr_id'+provider.id+'" class="tr-class-'+provider.id+'">'+
+        //        '<td id="td_id_'+provider.id+'" class="td-class-'+provider.id+'">'+provider.id+'</td>'+
+        //        '<td>'+provider.name+'</td>'+
+        //        '<td>'+provider.city+'</td>'+
+        //        '</tr>'
+        //    );
     };
 
     $.ajax({
@@ -22,4 +58,4 @@ function loadProvider() {
         url: "../../backend/application/index.php?rota=/loadProvider",
         success: success
     });
-}
+};
