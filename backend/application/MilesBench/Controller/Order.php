fbb7eb6fd5ab9e47eb5ff78c94e982d5bcbaaeb1
Roberto Martins <robertoalmartins@yahoo.com.br>
2015-05-14 15:44:06 -0300

    changes...

--- backend/application/MilesBench/Controller/Order.php
+++ backend/application/MilesBench/Controller/Order.php
@@ -244,7 +244,7 @@ class order {
         PHPMailerAutoload('SMTP');
         $mail = new PHPMailer;
 
-        $mail->SMTPDebug = 1;                               // Enable verbose debug output
+        $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
         $mail->isSMTP();                                      // Set mailer to use SMTP
         $mail->Host = 'smtp.mail.yahoo.com';                  // Specify main and backup SMTP servers
@@ -263,7 +263,7 @@ class order {
         //$mail->addBCC('bcc@example.com');
 
         //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-        $mail->addAttachment('../../../img/emissao_ideal.png');    // Optional name
+        $mail->addAttachment(dirname(__FILE__) . '../../../img/emissao_ideal.png');    // Optional name
         $mail->isHTML(true);                                  // Set email format to HTML
 
         $mail->Subject = 'Emissao de Bilhetes';
